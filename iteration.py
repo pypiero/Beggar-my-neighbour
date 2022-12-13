9bda4828657393538e5372cfdb213ec44a6c3f64
Pietro Andrigo <pietroandrigo@gmail.com>
2022-12-13 19:08:14 +0100

    grouped rows in a function

--- iteration.py
+++ iteration.py
@@ -10,28 +10,34 @@ def main_():
 	print(b)
 
 
-N_CARDS = 52
+
 
 #iteratore che restituisce combinazioni possibli
 def choose_n(n_cards,n_places):
 	return combinations(range(0,n_cards),n_places)
 	#print(b) # se printi srotoli la funzione
 
+def total_games():
 # restitusce numero di assi, due , tre e quattri
-aces = choose_n(N_CARDS,4)
-twos = choose_n(N_CARDS - 4,4)
-threes = choose_n(N_CARDS - 8,4)
-fourths = choose_n(N_CARDS - 12,4)
+	N_CARDS = 52
+	aces = choose_n(N_CARDS,4)
+	twos = choose_n(N_CARDS - 4,4)
+	threes = choose_n(N_CARDS - 8,4)
+	fourths = choose_n(N_CARDS - 12,4)
 
-#restituisce numero totale di combinazioni
-total = len(list(aces))*len(list(twos))*len(list(threes))*len(list(fourths))
-print('n totale di combinazioni: ', str(total))
-print('ordine di grandezza: ',len(str(total))-1)
+	#restituisce numero totale di combinazioni
+	total = len(list(aces))*len(list(twos))*len(list(threes))*len(list(fourths))
+	print('n totale di combinazioni: ', str(total))
+	print('ordine di grandezza: ',len(str(total))-1)
 
 #per capire se si può fare una lista enorme e consultare se il valore vale 1 o 0. 
 #questo serve a capire se abbiamo già fatto quella partita
 a = [0] * (10^20)
 print(a[10^15] == 1)
+
+
+total_games()
+
 
 '''
 TO IMPLEMENT:
