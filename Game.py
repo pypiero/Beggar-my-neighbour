633f68a2fa7cba4c9ea63a56e706760d625343be
Pietro Andrigo <pietroandrigo@gmail.com>
2022-12-17 18:31:58 +0100

    Removed the random function

--- Game.py
+++ Game.py
@@ -1,20 +1,4 @@
 #!/usr/bin/env python3
-import random
-
-'''
-function that returns a list representing a shuffle deck of 52 cards 
-where all the non-penality cards are '0' while J,Q,K, and A are 1,2,3,4.
-when italian_deck=True returns the italian 40 cards deck modified 
-'''
-def shuffled_deck(italian_deck=False):
-  if italian_deck: 
-    deck = 4*[1] + 4*[2] + 4*[3]  + 28*[0]
-  else:
-    deck = 4*[1] + 4*[2] + 4*[3] + 4*[4] + 36*[0]
-  random.shuffle(deck)
-  return deck
-
-
 ''' 
 function that simulate a beggar-my-neighborn game from a list of cards.
 returns the number of turn to end the game and the number of turns when
